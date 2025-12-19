0c0dc8d6c5041a280a8eb5ed7152094e76449a7d
Richard Hemba <richardhemba66@gmail.com>
2025-12-19 23:55:56 +0100

    ullllllll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import RootLayout from "./components/layout/RootLayout"
-
 import LoginInvestissementPage from "./pages/LoginInvestissementPage"
-// import UserInvest from "./pages/result"
 import InvestissementPage from "./pages/investire"
 import { Home } from "./pages"
 import UserInvest from "./pages/result"
@@ -10,23 +8,22 @@ import { hasToken } from "./utils/auth"
 import DashboardPage from "./pages/DashboardPage"
 
 const App: React.FC = () => {
-
   const isAuth = hasToken()
 
   return (
     <Routes>
-
+      {/* Routes principales avec layout */}
       <Route path="/" element={<RootLayout />}>
         <Route index element={isAuth ? <UserInvest /> : <Home />} />
-        <Route path="/conseil" element={<DashboardPage />} />
+        <Route path="conseil" element={<DashboardPage />} />
       </Route>
+
+      {/* Routes sans layout */}
       <Route path="/login" element={<LoginInvestissementPage />} />
       <Route path="/investir" element={<InvestissementPage />} />
 
-      {/* <Route path="/" element={<RootLayout />}>
-        <Route index element={<UserInvest />} />
-      </Route> */}
-
+      {/* Redirection pour les routes inconnues */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
