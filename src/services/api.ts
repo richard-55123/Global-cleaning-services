7d3e4e3bbae34a67e73110e4f2dde30041d2963c
Richard Hemba <richardhemba66@gmail.com>
2025-12-19 12:09:24 +0100

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://97a549589451.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true"
+    // "ngrok-skip-browser-warning": "false"
   }
 })
 
